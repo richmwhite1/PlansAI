4e84f37f6c6fc9c2fef6c6890465ffd6cb12c908
Rich White <richmwhite@gmail.com>
2026-02-13 15:21:52 -0700

    fix: next config and start script for standalone mode

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    ignoreBuildErrors: true,
-  },
 };
 
 export default nextConfig;
